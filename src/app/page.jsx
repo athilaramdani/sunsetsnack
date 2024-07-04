b936c9078b9977e4a2fadaad3446bffb39eb605d
athilaramdani <athilaramdani@gmail.com>
2024-07-04 14:55:16 +0700

    perbaruan

---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+import Navbar from '/src/components/navbar.jsx';
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <div>
+        <p>Testing</p>
+      </div>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="#">Navbar</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#"/>
+            <div className="collapse navbar-collapse" id="navbarNav"></div>
+        </nav>
+    )
+}
+
+export default Navbar

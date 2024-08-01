135f5247452fea839f92a131b2d2dd9fa694a4d2
Athila Ramdani Saputra <68465902+athilaramdani@users.noreply.github.com>
2024-08-01 11:50:48 +0700

    Delete src/components directory

--- src/components/ClientProvider.jsx
+++
@@ -1,8 +0,0 @@
-// src/components/ClientProvider.jsx
-"use client";
-
-import { SessionProvider } from 'next-auth/react';
-
-export default function ClientProvider({ children }) {
-  return <SessionProvider>{children}</SessionProvider>;
-}
